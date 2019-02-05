92222ba57fe5d8a0a51b2975049156b431da8952
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-05 14:45:45 +0100

    user controller done

--- src/main/java/com/mygroup/springbootcourse/Controller/UserController.java
+++ src/main/java/com/mygroup/springbootcourse/Controller/UserController.java
@@ -15,45 +15,33 @@ public class UserController {
 
     private List<User> users = new ArrayList<>();
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<User> registry(@RequestBody User user){
+//    @RequestMapping(value = "/registry", method = RequestMethod.POST)
+////    public ResponseEntity<User> registry(@RequestBody User user){
+////        users.add(user);
+////        return new ResponseEntity<>(user, HttpStatus.CREATED);
+////    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(value = "/registry", method = RequestMethod.POST)
+    public User registry(@RequestBody User user){
         users.add(user);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
-
-
+        return user;
     }
 
     @RequestMapping(value = "/edit", method = RequestMethod.PUT)
-    public void edit(){
-
-
-    }
-
-    @RequestMapping(value = "/confirm", method = RequestMethod.POST)
-    public void confirm(){
+    public User edit(@RequestBody User user){
+        return user;
 
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public User add(@RequestParam(value = "id" + "id2") Long id, Long id2){
-        System.out.println("buyCourse");
-        CourseController courseController = new CourseController();
-        courseController.buyCourse2(id2);
-        System.out.println(courseController.buyCourse2(id2));
-        return getUser(id);
+    @RequestMapping(value = "/confirm", method = RequestMethod.PUT)
+    public User confirm(@RequestParam(value = "id") Long id){
+        System.out.println("confrimed");
+        return new User();
     }
 
-    private User getUser(Long id) {
-        User user = null;
-        for (User u : users){
-                if(u.getId() !=null && u.getId().equals(id)){
-                    user = u;
-                    break;
-                }
-            }
-            if (user==null){
-                // TODO
-            }
-            return user;
-        }
+    @RequestMapping(value = "/add", method = RequestMethod.PUT)
+    public User add(@RequestBody User user){
+        return user;
+    }
 }
